92bd4c6ad92dad1cfc43126d18f2e5fea58b0d86
duckdaili25-arch <duckdaili25@gmail.com>
2026-05-24 03:05:19 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("NAPAS PROXY RUNNING 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
